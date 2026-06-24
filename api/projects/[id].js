f9e3595ca28c758137fde58d466b1e28eb62eba7
NULLZWASHERE <valdarvussy@gmail.com>
2026-06-24 00:44:41 -0400

    Update [id].js

--- api/projects/[id].js
+++ api/projects/[id].js
@@ -1,8 +1,15 @@
 export default async function handler(req, res) {
   const { id } = req.query;
 
+  const response = await fetch(
+    `https://scratch.mit.edu/projects/${id}/`
+  );
+
+  const html = await response.text();
+
   res.json({
-    id,
-    url: `https://scratch.mit.edu/projects/${id}/`
+    status: response.status,
+    length: html.length,
+    preview: html.slice(0, 500)
   });
 }
