eec624180b055af543f65a351eff6a06cbba84e0
NULLZWASHERE <valdarvussy@gmail.com>
2026-06-24 00:34:40 -0400

    Update [projects].js

--- api/scratch/[projects].js
+++ api/scratch/[projects].js
@@ -1,16 +1,16 @@
 export default async function handler(req, res) {
-  const { user } = req.query;
+  const { id } = req.query;
 
   const url = `https://scratch.mit.edu/projects/${id}/`;
 
   const response = await fetch(url, {
     headers: {
-      "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36"
+      "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36"
     }
   });
 
   const html = await response.text();
 
   res.setHeader("Content-Type", "text/html");
-  res.status(200).send(html);
+  res.status(response.status).send(html);
 }
